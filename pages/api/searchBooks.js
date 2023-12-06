93908b506ee6edc20fed6a75f15d0b1883eda09e
James Zhong <pzhong22@uwo.ca>
2023-12-06 17:14:11 -0500

    modified COR setup

--- pages/api/searchBooks.js
+++ pages/api/searchBooks.js
@@ -1,23 +1,5 @@
 import axios from 'axios';
-import Cors from 'cors';
-
-// Initializing the cors middleware with specific origin
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-  origin: 'http://localhost:3001', // Specify the client origin
-});
-
-// Helper method to initialize CORS and handle errors
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
+import cors from 'cors';
 
 // Asynchronous function to fetch books
 async function fetchBooks(query) {
@@ -31,28 +13,28 @@ async function fetchBooks(query) {
 }
 
 export default async function handler(req, res) {
-  // Run the middleware
-  await runMiddleware(req, res, cors);
+  // Apply CORS
+  await cors()(req, res, async () => {
+    const { searchTerm, category } = req.query;
+    try {
+      let query = category ? `subject:${category}` : searchTerm;
+      console.log(`Searching books for query: ${query}`);
 
-  const { searchTerm, category } = req.query;
-  try {
-    let query = category ? `subject:${category}` : searchTerm;
-    console.log(`Searching books for query: ${query}`);
+      const items = await fetchBooks(query);
 
-    const items = await fetchBooks(query);
+      const filteredBooks = items.filter(book => book.saleInfo && book.saleInfo.listPrice).map(book => ({
+        id: book.id,
+        title: book.volumeInfo.title,
+        authors: book.volumeInfo.authors,
+        thumbnail: book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : null,
+        description: book.volumeInfo.description,
+        price: book.saleInfo.listPrice.amount,
+        currencyCode: book.saleInfo.listPrice.currencyCode
+      }));
 
-    const filteredBooks = items.filter(book => book.saleInfo && book.saleInfo.listPrice).map(book => ({
-      id: book.id,
-      title: book.volumeInfo.title,
-      authors: book.volumeInfo.authors,
-      thumbnail: book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : null,
-      description: book.volumeInfo.description,
-      price: book.saleInfo.listPrice.amount,
-      currencyCode: book.saleInfo.listPrice.currencyCode
-    }));
-
-    res.json(filteredBooks);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching books' });
-  }
+      res.json(filteredBooks);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching books' });
+    }
+  });
 }
